c206149172f67b05a5ac21259f83ec23f087b9f8
Alejandro Suarez <101528810+suaracost@users.noreply.github.com>
2025-02-28 12:13:47 -0500

    Plantilla para otros servicios ✅

--- src/main/java/com/zephiro/content/security/SecurityConfig.java
+++ src/main/java/com/zephiro/content/security/SecurityConfig.java
@@ -1,16 +1,25 @@
 package com.zephiro.content.security;
 // ToDo: para el servicio plantilla
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    
+    @Autowired
+    private JWTAuthEntryPoint jwtAuthEntryPoint;
     
     @Bean 
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -19,7 +28,29 @@ public class SecurityConfig {
             .headers(headers -> headers.frameOptions(frame -> frame.disable()))
             .authorizeHttpRequests(requests -> requests
                 .requestMatchers("/h2/**").permitAll()
-                .anyRequest().permitAll());
+                .anyRequest().authenticated()
+            )
+            .exceptionHandling(exception -> exception.authenticationEntryPoint(jwtAuthEntryPoint));
+
+            http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+
         return http.build();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    
+    @Bean
+    public AuthenticationManager authenticationManager(
+        AuthenticationConfiguration authenticationConfiguration
+    ) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+    
+    @Bean
+    public JWTAuthenticationFilter jwtAuthenticationFilter() {
+        return new JWTAuthenticationFilter();
     }
 }
